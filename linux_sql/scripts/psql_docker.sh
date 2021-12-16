7f01dada809674eb6e0556a47d7aa5a1ac301c9e
Andrew Chew <andrewchew.ym@gmail.com>
2021-12-16 21:08:46 +0000

    Completed psql_docker.sh script to create|start|stop the psql container

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -1,53 +1,62 @@
 #! /bin/sh
 
-#capture CLI arguments (please do not copy comments)
+# Capture CLI arguments.
 cmd=$1
 db_username=$2
 db_password=$3
 
-#Start docker
-#Make sure you understand `||` cmd
-sudo systemctl status docker || systemctl ...
+# Check if docker is running, start it otherwise.
+sudo systemctl status docker || sudo systemctl start docker
 
-#check container status (try the following cmds on terminal)
+# Check container status.
 docker container inspect jrvs-psql
 container_status=$?
 
-#User switch case to handle create|stop|start opetions
+# Case statement to handle create, start, and stop operations.
 case $cmd in 
   create)
   
-  # Check if the container is already created
+  # Exit if container has already been created.
   if [ $container_status -eq 0 ]; then
 		echo 'Container already exists'
 		exit 1	
 	fi
 
-  #check # of CLI arguments
+  # Check # of CLI arguments.
   if [ $# -ne 3 ]; then
     echo 'Create requires username and password'
     exit 1
   fi
   
-  #Create container
-	docker volume ....
-	docker run ....
+  # Create container if it doesn't exist.
+	docker volume create pgdata
+	docker run \
+	  --name jrvs-psql \
+	  -e POSTGRES_USER=$db_username \
+	  -e POSTGRES_PASSWORD=$db_password \
+	  -d \
+	  -v pgdata:/var/lib/postgresql/data \
+	  -p 5432:5432 \
+	  postgres:9.6-alpine
 	exit $?
 	;;
 
-  start|stop) 
-  #check instance status; exit 1 if container has not been created
-  if [ $container_status ...
-  ...
+  start|stop)
 
-  #Start or stop the container
+  # Check container status; exit 1 if container has not been created.
+  if [ $container_status -ne 0 ]; then
+    echo 'Container has not been created'
+    exit 1
+  fi
+
+  # Start or stop the container
 	docker container $cmd jrvs-psql
 	exit $?
-	;;	
+	;;
   
   *)
 	echo 'Illegal command'
 	echo 'Commands: start|stop|create'
 	exit 1
 	;;
-esac 
+esac
