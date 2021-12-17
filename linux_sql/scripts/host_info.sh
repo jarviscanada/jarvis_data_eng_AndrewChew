ef34e47a9186b28924b99d7b4e2b3807b75e812b
Andrew Chew <andrewchew.ym@gmail.com>
2021-12-17 20:27:42 +0000

    Implemented host_info.sh script

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -1,40 +1,43 @@
-#Setup and validate arguments (again, don't copy comments)
+#! /bin/bash
+
+# Check # args.
+if [ "$#" -ne 5 ]; then
+  echo "Illegal number of parameters"
+  echo "host_info requires: psql_host, psql_port, db_name, psql_user, psql_password"
+  exit 1
+fi
+
+# Setup arguments.
 psql_host=$1
 psql_port=$2
 db_name=$3
 psql_user=$4
 psql_password=$5
 
-#Check # of args
-if [ $# -ne  ...
-  ...
-  exit 1
-fi
-
-#Save machine statistics in MB and current machine hostname to variables
-vmstat_mb=$(vmstat --unit M)
+# Save machine statistics to variables.
 hostname=$(hostname -f)
+lscpu_out=$(lscpu)
+vmstat_t=$(vmstat -t)
 
-#Retrieve hardware specification variables
-#xargs is a trick to trim leading and trailing white spaces
-memory_free=$(echo "$vmstat_mb" | awk '{print $4}'| tail -n1 | xargs)
-cpu_idle=$(echo "$vmstat_mb" ...
-cpu_kernel=$(echo "$vmstat_mb" ...
-disk_io=$(vmstat -d | awk '{print $10}' ...
-disk_available=$(df -BM / ...
+# Retrieve hardware specification variables.
+cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out" | egrep "^Model name:" | awk '{for (i=3; i<=NF; i++) print $i}' | xargs)
+cpu_mhz=$(echo "$lscpu_out"  | egrep "^CPU MHz:" | awk '{print $3}' | xargs)
+l2_cache=$(echo "$lscpu_out"  | egrep "^L2 cache:" | awk '{print $3}' | sed 's/K//' | xargs)
+total_mem=$(head -1 /proc/meminfo | awk '{print $2}' | xargs)
 
-#Current time in `2019-11-26 14:40:19` UTC format
-timestamp=$(vmstat -t | awk ...
+# Retrieve current time in `2019-11-26 14:40:19` UTC format.
+timestamp=$(echo "$vmstat_t" | tail -1 | awk '{print $18 " "  $19}' | xargs)
 
-#Subquery to find matching id in host_info table
-host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
+# PSQL command: Inserts server usage data into host_info table.
+insert_stmt="INSERT INTO host_info \
+  VALUES(DEFAULT, '$hostname', $cpu_number, '$cpu_architecture',\
+        '$cpu_model', $cpu_mhz, $l2_cache, $total_mem, '$timestamp')"
 
-#PSQL command: Inserts server usage data into host_usage table
-#Note: be careful with double and single quotes
-insert_stmt="INSERT INTO host_usage(timestamp, ...) VALUES('$timestamp', ..."
-
-#set up env var for pql cmd
+# Set up PGPASSWORD environment variable to connect to psql instance without prompting for password.
 export PGPASSWORD=$psql_password
-#Insert date into a database
+
+#Insert data into the database.
 psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
 exit $?
