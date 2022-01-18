0f71a08b9c6f7f13721c5b97de6a124b1a6d629d
Andrew Chew <andrewchew.ym@gmail.com>
2022-01-18 20:39:33 +0000

    Updated CustomerDAO and JDBCExecutor to Read Customers from the database.

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/CustomerDAO.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/CustomerDAO.java
@@ -3,6 +3,7 @@ package ca.jrvs.apps.jdbc;
 import ca.jrvs.apps.jdbc.util.DataAccessObject;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -11,13 +12,35 @@ public class CustomerDAO extends DataAccessObject<Customer> {
   private static final String INSERT = "INSERT INTO customer (first_name, last_name, email, phone, "
       + "address, city, state, zipcode) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
+  private static final String GET_ONE = "SELECT customer_id, first_name, last_name, email, phone, "
+      + "address, city, state, zipcode FROM customer WHERE customer_id = ?";
+
   public CustomerDAO(Connection connection) {
     super(connection);
   }
 
   @Override
   public Customer findById(long id) {
-    return null;
+    Customer customer = new Customer();
+    try (PreparedStatement statement = this.connection.prepareStatement(GET_ONE)) {
+      statement.setLong(1, id);
+      ResultSet resultSet = statement.executeQuery();
+      while (resultSet.next()) {
+        customer.setId(resultSet.getLong("customer_id"));
+        customer.setFirstName(resultSet.getString("first_name"));
+        customer.setLastName(resultSet.getString("last_name"));
+        customer.setEmail(resultSet.getString("email"));
+        customer.setPhone(resultSet.getString("phone"));
+        customer.setAddress(resultSet.getString("address"));
+        customer.setCity(resultSet.getString("city"));
+        customer.setState(resultSet.getString("state"));
+        customer.setZipCode(resultSet.getString("zipcode"));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw new RuntimeException(e);
+    }
+    return customer;
   }
 
   @Override
@@ -32,7 +55,7 @@ public class CustomerDAO extends DataAccessObject<Customer> {
 
   @Override
   public Customer create(Customer dto) {
-    try (PreparedStatement statement = this.connection.prepareStatement(INSERT);) {
+    try (PreparedStatement statement = this.connection.prepareStatement(INSERT)) {
       statement.setString(1, dto.getFirstName());
       statement.setString(2, dto.getLastName());
       statement.setString(3, dto.getEmail());
@@ -42,7 +65,8 @@ public class CustomerDAO extends DataAccessObject<Customer> {
       statement.setString(7, dto.getState());
       statement.setString(8, dto.getZipCode());
       statement.execute();
-      return null;
+      int id = this.getLastVal(CUSTOMER_SEQUENCE);
+      return this.findById(id);
     } catch (SQLException e) {
       e.printStackTrace();
       throw new RuntimeException(e);
--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -13,17 +13,8 @@ public class JDBCExecutor {
     try {
       Connection connection = dcm.getConnection();
       CustomerDAO customerDAO = new CustomerDAO(connection);
-      Customer customer = new Customer();
-      customer.setFirstName("George");
-      customer.setLastName("Washington");
-      customer.setEmail("george.washington@wh.gov");
-      customer.setPhone("(555) 555-6543");
-      customer.setAddress("1234 Main St");
-      customer.setCity("Mount Vernon");
-      customer.setState("VA");
-      customer.setZipCode("22121");
-
-      customerDAO.create(customer);
+      Customer customer = customerDAO.findById(1000);
+      System.out.println(customer.getFirstName() + " " + customer.getLastName());
     } catch (SQLException e) {
       e.printStackTrace();
     }
