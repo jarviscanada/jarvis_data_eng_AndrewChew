0bb98aa91ce4567b28bff094be14af7c24a1bb29
Andrew Chew <andrewchew.ym@gmail.com>
2022-01-19 17:07:03 +0000

    Updated update method in CustomerDAO to commit manually and have rollback functionality if the transaction fails.

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/CustomerDAO.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/CustomerDAO.java
@@ -56,6 +56,12 @@ public class CustomerDAO extends DataAccessObject<Customer> {
   @Override
   public Customer update(Customer dto) {
     Customer customer;
+    try {
+      this.connection.setAutoCommit(false);
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw new RuntimeException(e);
+    }
     try (PreparedStatement statement = this.connection.prepareStatement(UPDATE)) {
       statement.setString(1, dto.getFirstName());
       statement.setString(2, dto.getLastName());
@@ -67,8 +73,15 @@ public class CustomerDAO extends DataAccessObject<Customer> {
       statement.setString(8, dto.getZipCode());
       statement.setLong(9, dto.getId());
       statement.execute();
+      this.connection.commit();
       customer = this.findById(dto.getId());
     } catch (SQLException e) {
+      try {
+        this.connection.rollback();
+      } catch (SQLException sqlException) {
+        sqlException.printStackTrace();
+        throw new RuntimeException(sqlException);
+      }
       e.printStackTrace();
       throw new RuntimeException(e);
     }
