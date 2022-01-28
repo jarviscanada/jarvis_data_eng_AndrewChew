9ca63ea7b88cd1ff130ca2a0dfc4638a4cff6a55
Andrew Chew <andrewchew.ym@gmail.com>
2022-01-28 19:56:16 +0000

    Added ignore properties annotation to handle unrecognized fields.

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Entities.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Entities.java
@@ -1,9 +1,11 @@
 package ca.jrvs.apps.twitter.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(Include.NON_NULL)
 public class Entities {
 
--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Tweet.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Tweet.java
@@ -1,10 +1,12 @@
 package ca.jrvs.apps.twitter.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(Include.NON_NULL)
 @JsonPropertyOrder({
     "created_at",
