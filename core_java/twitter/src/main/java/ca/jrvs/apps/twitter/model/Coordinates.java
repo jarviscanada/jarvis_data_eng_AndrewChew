50236bfd6744d2ab08f4427d4ebe629988f4ca8a
Andrew Chew <andrewchew.ym@gmail.com>
2022-01-28 18:34:30 +0000

    Fixed coordinate field type to a List of Doubles.

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Coordinates.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Coordinates.java
@@ -2,18 +2,19 @@ package ca.jrvs.apps.twitter.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import java.util.List;
 
 @JsonInclude(Include.NON_NULL)
 public class Coordinates {
 
-  private float[] coordinates;
+  private List<Double> coordinates;
   private String type;
 
-  public float[] getCoordinates() {
+  public List<Double> getCoordinates() {
     return coordinates;
   }
 
-  public void setCoordinates(float[] coordinates) {
+  public void setCoordinates(List<Double> coordinates) {
     this.coordinates = coordinates;
   }
 
