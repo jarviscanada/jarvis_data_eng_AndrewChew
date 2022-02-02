c7c688b9d175671d6f055336319f56c5ed21d8bc
Andrew Chew <andrewchew.ym@gmail.com>
2022-02-02 19:19:01 +0000

    Implemented integration tests for postTweet.

--- core_java/twitter/src/test/java/ca/jrvs/apps/twitter/controller/TwitterControllerIntTest.java
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/controller/TwitterControllerIntTest.java
@@ -5,17 +5,21 @@ import static org.junit.Assert.*;
 import ca.jrvs.apps.twitter.dao.TwitterDao;
 import ca.jrvs.apps.twitter.dao.helper.HttpHelper;
 import ca.jrvs.apps.twitter.dao.helper.TwitterHttpHelper;
+import ca.jrvs.apps.twitter.model.Tweet;
 import ca.jrvs.apps.twitter.service.TwitterService;
 import java.util.HashMap;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TwitterControllerIntTest {
 
   private static TwitterController twitterController;
 
   @BeforeClass
-  public void setUp() throws Exception {
+  public static void setUp() throws Exception {
     String consumerKey = System.getenv("consumerKey");
     String consumerSecret = System.getenv("consumerSecret");
     String accessToken = System.getenv("accessToken");
@@ -30,14 +34,40 @@ public class TwitterControllerIntTest {
   }
 
   @Test
-  public void postTweet() {
+  public void firstPostTweetSuccess() {
+    String[] args = {"post", "test tweet", "1:-1"};
+    Double expectedLon = Double.parseDouble(args[2].split(":")[1]);
+    Double expectedLat = Double.parseDouble(args[2].split(":")[0]);
+    Tweet actual = twitterController.postTweet(args);
+    assertNotNull(actual);
+    assertEquals(args[1], actual.getText());
+    assertEquals(expectedLon, actual.getCoordinates().getCoordinates().get(0));
+    assertEquals(expectedLat, actual.getCoordinates().getCoordinates().get(1));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void firstPostTweetFailureWrongNumArgs() {
+    String[] args = {"test tweet"};
+    twitterController.postTweet(args);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void firstPostTweetFailureWrongNumCoords() {
+    String[] args = {"post", "test tweet", "1:1:1"};
+    twitterController.postTweet(args);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void firstPostTweetFailureInvalidCoords() {
+    String[] args = {"post", "test tweet", "-10000:10"};
+    twitterController.postTweet(args);
   }
 
   @Test
-  public void showTweet() {
+  public void secondShowTweetSuccess() {
   }
 
   @Test
-  public void deleteTweet() {
+  public void thirdDeleteTweetSuccess() {
   }
 }
