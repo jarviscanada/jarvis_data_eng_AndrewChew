ec16f39e94ff8f9c2b3d973dcc6727faceffa2c3
Andrew Chew <andrewchew.ym@gmail.com>
2022-02-02 15:04:26 +0000

    Implemented integration tests for showTweet.

--- core_java/twitter/src/test/java/ca/jrvs/apps/twitter/service/TwitterServiceIntTest.java
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/service/TwitterServiceIntTest.java
@@ -4,22 +4,25 @@ import static org.junit.Assert.*;
 
 import ca.jrvs.apps.twitter.dao.TwitterDao;
 import ca.jrvs.apps.twitter.dao.helper.HttpHelper;
+import ca.jrvs.apps.twitter.dao.helper.JsonUtil;
 import ca.jrvs.apps.twitter.dao.helper.TweetUtil;
 import ca.jrvs.apps.twitter.dao.helper.TwitterHttpHelper;
 import ca.jrvs.apps.twitter.model.Tweet;
-import java.awt.TextComponent;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TwitterServiceIntTest {
 
   private static TwitterService twitterService;
-  private static List<String> ids;
+  private static Map<Integer, String> ids;
 
   @BeforeClass
-  public static void setUp() throws Exception {
+  public static void setUp() {
     String consumerKey = System.getenv("consumerKey");
     String consumerSecret = System.getenv("consumerSecret");
     String accessToken = System.getenv("accessToken");
@@ -30,11 +33,11 @@ public class TwitterServiceIntTest {
         accessToken, tokenSecret);
     TwitterDao twitterDao = new TwitterDao(httpHelper);
     twitterService = new TwitterService(twitterDao);
-    ids = new ArrayList<>();
+    ids = new HashMap<>();
   }
 
   @Test
-  public void postTweetSuccessTweetUtil() {
+  public void firstPostTweetSuccessTweetUtil() {
     String text = "testing tweet";
     Double lon = 1.0;
     Double lat = -1.0;
@@ -44,11 +47,11 @@ public class TwitterServiceIntTest {
     assertEquals(text, actual.getText());
     assertEquals(lon, actual.getCoordinates().getCoordinates().get(0));
     assertEquals(lat, actual.getCoordinates().getCoordinates().get(1));
-    ids.add(actual.getId_str());
+    ids.put(1, actual.getId_str());
   }
 
   @Test
-  public void postTweetSuccessManual() {
+  public void firstPostTweetSuccessManual() {
     String text = "testing tweet manually";
     Tweet tweet = new Tweet();
     tweet.setText(text);
@@ -57,18 +60,18 @@ public class TwitterServiceIntTest {
     assertNotNull(actual.getId_str());
     assertNull(actual.getCoordinates());
     assertEquals(text, actual.getText());
-    ids.add(actual.getId_str());
+    ids.put(2, actual.getId_str());
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void postTweetFailureNoText() {
+  public void firstPostTweetFailureNoText() {
     Double lon = 1.0;
     Double lat = -1.0;
     twitterService.postTweet(TweetUtil.buildTweet("", lon, lat));
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void postTweetFailureLongText() {
+  public void firstPostTweetFailureLongText() {
     String longText = "This is some ridiculously long text that I am coming up with here."
         + "It really has no business being this long other than to test my app when handling"
         + " ridiculously long texts such as the one I am coming up with here. I think this "
@@ -82,24 +85,103 @@ public class TwitterServiceIntTest {
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void postTweetFailureBadLongitude() {
+  public void firstPostTweetFailureBadLongitude() {
     Double lon = 1000.0;
     Double lat = -1.0;
     twitterService.postTweet(TweetUtil.buildTweet("", lon, lat));
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void postTweetFailureBadLatitude() {
+  public void firstPostTweetFailureBadLatitude() {
     Double lon = 1.0;
     Double lat = -1000.0;
     twitterService.postTweet(TweetUtil.buildTweet("", lon, lat));
   }
 
   @Test
-  public void showTweet() {
+  public void secondShowTweetSuccessAllFields() throws Exception{
+    String[] fields = {};
+    Tweet actual1 = twitterService.showTweet(ids.get(1), fields);
+    Tweet actual2 = twitterService.showTweet(ids.get(2), fields);
+
+    assertNotNull(actual1);
+    assertNotNull(actual1.getCreated_at());
+    assertNotNull(actual1.getId());
+    assertNotNull(actual1.getId_str());
+    assertEquals(ids.get(1), actual1.getId_str());
+    assertNotNull(actual1.getText());
+    assertNotNull(actual1.getEntities());
+    assertNotNull(actual1.getCoordinates());
+    assertNotNull(actual1.getRetweet_count());
+    assertNotNull(actual1.getFavorite_count());
+    assertNotNull(actual1.isFavorited());
+    assertNotNull(actual1.isRetweeted());
+
+    assertNotNull(actual2);
+    assertNotNull(actual2.getCreated_at());
+    assertNotNull(actual2.getId());
+    assertNotNull(actual2.getId_str());
+    assertEquals(ids.get(2), actual2.getId_str());
+    assertNotNull(actual2.getText());
+    assertNotNull(actual2.getEntities());
+    assertNull(actual2.getCoordinates());
+    assertNotNull(actual2.getRetweet_count());
+    assertNotNull(actual2.getFavorite_count());
+    assertNotNull(actual2.isFavorited());
+    assertNotNull(actual2.isRetweeted());
+
+    System.out.println(JsonUtil.toPrettyJson(actual1));
+    System.out.println(JsonUtil.toPrettyJson(actual2));
   }
 
   @Test
-  public void deleteTweets() {
+  public void secondShowTweetSuccessSelectFields() throws Exception {
+    String[] fields = {"id_str", "text"};
+    Tweet actual1 = twitterService.showTweet(ids.get(1), fields);
+    Tweet actual2 = twitterService.showTweet(ids.get(2), fields);
+
+    assertNotNull(actual1);
+    assertNull(actual1.getCreated_at());
+    assertNull(actual1.getId());
+    assertNotNull(actual1.getId_str());
+    assertEquals(ids.get(1), actual1.getId_str());
+    assertNotNull(actual1.getText());
+    assertNull(actual1.getEntities());
+    assertNull(actual1.getCoordinates());
+    assertNull(actual1.getRetweet_count());
+    assertNull(actual1.getFavorite_count());
+    assertNull(actual1.isFavorited());
+    assertNull(actual1.isRetweeted());
+
+    assertNotNull(actual2);
+    assertNull(actual2.getCreated_at());
+    assertNull(actual2.getId());
+    assertNotNull(actual2.getId_str());
+    assertEquals(ids.get(2), actual2.getId_str());
+    assertNotNull(actual2.getText());
+    assertNull(actual2.getEntities());
+    assertNull(actual2.getCoordinates());
+    assertNull(actual2.getRetweet_count());
+    assertNull(actual2.getFavorite_count());
+    assertNull(actual2.isFavorited());
+    assertNull(actual2.isRetweeted());
+
+    System.out.println(JsonUtil.toPrettyJson(actual1));
+    System.out.println(JsonUtil.toPrettyJson(actual2));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void secondShowTweetFailureBadId() {
+    String[] fields = {};
+    twitterService.showTweet("someid", fields);
+  }
+
+  @Test
+  public void thirdDeleteTweetSuccess() {
+  }
+
+  @Test
+  public void thirdDeleteTweetFailure() {
+
   }
 }
