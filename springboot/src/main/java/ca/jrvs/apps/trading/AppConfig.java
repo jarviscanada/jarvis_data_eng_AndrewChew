fac12c9022954ac5cd58fc43d5aa88bc2fab676d
Andrew Chew <andrewchew.ym@gmail.com>
2022-03-04 19:54:02 +0000

    Implemented Application and AppConfig.

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/AppConfig.java
@@ -0,0 +1,36 @@
+package ca.jrvs.apps.trading;
+
+import ca.jrvs.apps.trading.model.config.MarketDataConfig;
+import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * AppConfig is responsible for configuring beans.
+ */
+@Configuration
+public class AppConfig {
+
+  private Logger logger = LoggerFactory.getLogger(AppConfig.class);
+
+  @Bean
+  public MarketDataConfig marketDataConfig() {
+    MarketDataConfig marketDataConfig = new MarketDataConfig();
+    marketDataConfig.setHost("https://cloud.iexapis.com/v1");
+    marketDataConfig.setToken(System.getenv("IEX_PUB_TOKEN"));
+    return marketDataConfig;
+  }
+
+  @Bean
+  public HttpClientConnectionManager httpClientConnectionManager() {
+    PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
+    cm.setMaxTotal(50);
+    cm.setDefaultMaxPerRoute(50);
+    return cm;
+  }
+}
