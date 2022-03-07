2dae2ac76ffd3796d51f1f55f26e3196402a6b42
Andrew Chew <andrewchew.ym@gmail.com>
2022-03-07 16:38:31 -0500

    Added starter code for QuoteDao.

--- springboot/src/main/java/ca/jrvs/apps/trading/dao/QuoteDao.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/dao/QuoteDao.java
@@ -1,5 +1,160 @@
 package ca.jrvs.apps.trading.dao;
 
-public class QuoteDao {
+import ca.jrvs.apps.trading.model.domain.Quote;
+import java.util.List;
+import java.util.Optional;
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataRetrievalFailureException;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
 
+/**
+ * QuoteDao is responsible for CRUD to and from the database.
+ */
+@Repository
+public class QuoteDao implements CrudRepository<Quote, String> {
+
+  private static final String TABLE_NAME = "quote";
+  private static final String ID_COLUMN_NAME = "ticker";
+
+  private static final Logger logger = LoggerFactory.getLogger(QuoteDao.class);
+  private JdbcTemplate jdbcTemplate;
+  private SimpleJdbcInsert simpleJdbcInsert;
+
+  @Autowired
+  public QuoteDao(DataSource dataSource) {
+    jdbcTemplate = new JdbcTemplate(dataSource);
+    simpleJdbcInsert = new SimpleJdbcInsert(dataSource).withTableName(TABLE_NAME);
+  }
+
+  /**
+   * Update or save a quote.
+   *
+   * @param quote to save
+   * @return quote saved
+   * @throws DataAccessException for unexpected SQL result or SQL execution failure
+   */
+  @Override
+  public Quote save(Quote quote) {
+    if (existsById(quote.getId())) {
+      int updatedRowNo = updateOne(quote);
+      if (updatedRowNo != 1) {
+        throw new DataRetrievalFailureException("Unable to update quote");
+      }
+    } else {
+      addOne(quote);
+    }
+    return quote;
+  }
+
+  /**
+   * Helper method that saves one quote.
+   *
+   * @param quote to save
+   */
+  private void addOne(Quote quote) {
+    SqlParameterSource parameterSource = new BeanPropertySqlParameterSource(quote);
+    int row = simpleJdbcInsert.execute(parameterSource);
+    if (row != 1) {
+      throw new IncorrectResultSizeDataAccessException("Failed to insert", 1, row);
+    }
+  }
+
+  /**
+   * Helper method that updates a quote.
+   *
+   * @param quote to update
+   */
+  private int updateOne(Quote quote) {
+    String updateSql = "UPDATE quote SET last_price=?, bid_price=?, "
+        + "bid_size=?, ask_price=?, ask_size=? WHERE ticker=?";
+    return jdbcTemplate.update(updateSql, makeUpdateValues(quote));
+  }
+
+  /**
+   * Helper method that makes sql update value objects.
+   *
+   * @param quote with updated values
+   * @return sql update value objects
+   */
+  private Object[] makeUpdateValues(Quote quote) {
+    // TODO
+    return null;
+  }
+
+  @Override
+  public <S extends Quote> List<S> saveAll(Iterable<S> quotes) {
+    // TODO
+    return null;
+  }
+
+  /**
+   * Find a quote by ticker.
+   *
+   * @param ticker id of quote to find
+   * @return quote or Optional.empty() if not found
+   */
+  @Override
+  public Optional<Quote> findById(String ticker) {
+    // TODO
+    return Optional.empty();
+  }
+
+  @Override
+  public boolean existsById(String s) {
+    // TODO
+    return false;
+  }
+
+  /**
+   * Returns all quotes.
+   *
+   * @return List of all quotes
+   * @throws DataAccessException if failed to update
+   */
+  @Override
+  public List<Quote> findAll() {
+    // TODO
+    return null;
+  }
+
+  @Override
+  public Iterable<Quote> findAllById(Iterable<String> iterable) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  @Override
+  public long count() {
+    // TODO
+    return 0;
+  }
+
+  @Override
+  public void deleteById(String ticker) {
+    // TODO
+  }
+
+  @Override
+  public void delete(Quote quote) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  @Override
+  public void deleteAll(Iterable<? extends Quote> iterable) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  @Override
+  public void deleteAll() {
+    // TODO
+  }
 }
