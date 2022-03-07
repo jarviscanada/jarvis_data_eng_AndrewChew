b660155de746070e536dffb706844f50cd9736de
Andrew Chew <andrewchew.ym@gmail.com>
2022-03-07 14:50:42 -0500

    Created a data source.

--- springboot/src/main/java/ca/jrvs/apps/trading/AppConfig.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/AppConfig.java
@@ -1,12 +1,12 @@
 package ca.jrvs.apps.trading;
 
 import ca.jrvs.apps.trading.model.config.MarketDataConfig;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.http.conn.HttpClientConnectionManager;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +17,24 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
   private Logger logger = LoggerFactory.getLogger(AppConfig.class);
+
+  @Bean
+  public DataSource dataSource() {
+    String jdbcUrl =
+        "jdbc:postgresql://"
+            + System.getenv("PSQL_HOST") + ":"
+            + System.getenv("PSQL_PORT") + "/"
+            + System.getenv("PSQL_DB");
+    String user = System.getenv("PSQL_USER");
+    String password = System.getenv("PSQL_PASSWORD");
+
+    // Never log your credentials/secrets. Use IDE debugger instead.
+    BasicDataSource basicDataSource = new BasicDataSource();
+    basicDataSource.setUrl(jdbcUrl);
+    basicDataSource.setUsername(user);
+    basicDataSource.setPassword(password);
+    return basicDataSource;
+  }
 
   @Bean
   public MarketDataConfig marketDataConfig() {
