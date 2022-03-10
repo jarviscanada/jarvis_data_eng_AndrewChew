1267c83285a592181b8478d5e948f5aad2fbeb53
Andrew Chew <andrewchew.ym@gmail.com>
2022-03-10 16:16:25 -0500

    Implemented TraderDao.

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/dao/TraderDao.java
@@ -0,0 +1,59 @@
+package ca.jrvs.apps.trading.dao;
+
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TraderDao extends JdbcCrudDao<Trader> {
+
+  private static final Logger logger = LoggerFactory.getLogger(TraderDao.class);
+
+  private final String TABLE_NAME = "trader";
+  private final String ID_COLUMN = "id";
+
+  private JdbcTemplate jdbcTemplate;
+  private SimpleJdbcInsert simpleInsert;
+
+  @Autowired
+  public TraderDao(DataSource dataSource) {
+    this.jdbcTemplate = new JdbcTemplate(dataSource);
+    this.simpleInsert = new SimpleJdbcInsert(dataSource)
+        .withTableName(TABLE_NAME)
+        .usingGeneratedKeyColumns(ID_COLUMN);
+  }
+
+  @Override
+  public JdbcTemplate getJdbcTemplate() {
+    return jdbcTemplate;
+  }
+
+  @Override
+  public SimpleJdbcInsert getSimpleJdbcInsert() {
+    return simpleInsert;
+  }
+
+  @Override
+  public String getTableName() {
+    return TABLE_NAME;
+  }
+
+  @Override
+  public String getIdColumnName() {
+    return ID_COLUMN;
+  }
+
+  @Override
+  Class<Trader> getEntityClass() {
+    return Trader.class;
+  }
+
+  @Override
+  public int updateOne(Trader entity) {
+    throw new UnsupportedOperationException("Note implemented");
+  }
+}
