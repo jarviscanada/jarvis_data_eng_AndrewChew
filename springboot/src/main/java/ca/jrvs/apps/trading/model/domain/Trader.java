93cf2767bdcfae227442d9ac6a88dd3be4151f41
Andrew Chew <andrewchew.ym@gmail.com>
2022-03-10 16:25:39 -0500

    Implemented Trader.

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/model/domain/Trader.java
@@ -0,0 +1,61 @@
+package ca.jrvs.apps.trading.model.domain;
+
+public class Trader implements Entity<Integer> {
+
+  private Integer id;
+  private String country;
+  private String dob;
+  private String email;
+  private String firstName;
+  private String lastName;
+
+  @Override
+  public Integer getId() {
+    return id;
+  }
+
+  @Override
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  public String getDob() {
+    return dob;
+  }
+
+  public void setDob(String dob) {
+    this.dob = dob;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+}
