92764c7dffa8834a96c50a5fbdfdf9059b4f68e0
Andrew Chew <andrewchew.ym@gmail.com>
2022-03-10 15:33:40 -0500

    Added starter code for JdbcCrudDao.

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/dao/JdbcCrudDao.java
@@ -0,0 +1,121 @@
+package ca.jrvs.apps.trading.dao;
+
+import ca.jrvs.apps.trading.model.domain.Entity;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataRetrievalFailureException;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+public abstract class JdbcCrudDao<T extends Entity<Integer>> implements CrudRepository<T, Integer> {
+
+  private static final Logger logger = LoggerFactory.getLogger(JdbcCrudDao.class);
+
+  abstract public JdbcTemplate getJdbcTemplate();
+
+  abstract public SimpleJdbcInsert getSimpleJdbcInsert();
+
+  abstract public String getTableName();
+
+  abstract public String getIdColumnName();
+
+  abstract Class<T> getEntityClass();
+
+  /**
+   * Save an entity and update auto-generated integer ID.
+   *
+   * @param entity to save
+   * @return saved entity
+   */
+  @Override
+  public <S extends T> S save(S entity) {
+    if (existsById(entity.getId())) {
+      if (updateOne(entity) != 1) {
+        throw new DataRetrievalFailureException("Unable to update entity");
+      }
+    } else {
+      addOne(entity);
+    }
+    return entity;
+  }
+
+  /**
+   * Helper method to save an entity.
+   */
+  private <S extends T> void addOne(S entity) {
+    SqlParameterSource parameterSource = new BeanPropertySqlParameterSource(entity);
+    Number newId = getSimpleJdbcInsert().executeAndReturnKey(parameterSource);
+    entity.setId(newId.intValue());
+  }
+
+  /**
+   * Helper method to update an entity.
+   */
+  abstract public int updateOne(T entity);
+
+  @Override
+  public <S extends T> Iterable<S> saveAll(Iterable<S> iterable) {
+    return null;
+  }
+
+  @Override
+  public Optional<T> findById(Integer id) {
+    Optional<T> entity = Optional.empty();
+    String selectSql = "SELECT * FROM " + getTableName() + " WHERE " + getIdColumnName() + " =?";
+
+    try {
+      entity = Optional.ofNullable((T) getJdbcTemplate()
+          .queryForObject(selectSql,
+              BeanPropertyRowMapper.newInstance(getEntityClass()), id));
+    } catch (IncorrectResultSizeDataAccessException e) {
+      logger.debug("Can't find entity id: " + id, e);
+    }
+    return entity;
+  }
+
+  @Override
+  public boolean existsById(Integer integer) {
+    return false;
+  }
+
+  @Override
+  public Iterable<T> findAll() {
+    return null;
+  }
+
+  @Override
+  public Iterable<T> findAllById(Iterable<Integer> iterable) {
+    return null;
+  }
+
+  @Override
+  public long count() {
+    return 0;
+  }
+
+  @Override
+  public void deleteById(Integer integer) {
+
+  }
+
+  @Override
+  public void delete(T t) {
+
+  }
+
+  @Override
+  public void deleteAll(Iterable<? extends T> iterable) {
+
+  }
+
+  @Override
+  public void deleteAll() {
+
+  }
+}
