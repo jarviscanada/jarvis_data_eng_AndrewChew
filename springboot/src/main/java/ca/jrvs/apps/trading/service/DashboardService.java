8b71b1dd952d3fb329362f44aa7a7f67696627dd
Andrew Chew <andrewchew.ym@gmail.com>
2022-03-14 17:24:17 -0400

    Implemented getProfileViewByTraderId method.

--- springboot/src/main/java/ca/jrvs/apps/trading/service/DashboardService.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/service/DashboardService.java
@@ -6,8 +6,13 @@ import ca.jrvs.apps.trading.dao.QuoteDao;
 import ca.jrvs.apps.trading.dao.TraderDao;
 import ca.jrvs.apps.trading.model.domain.Account;
 import ca.jrvs.apps.trading.model.domain.PortfolioView;
+import ca.jrvs.apps.trading.model.domain.Position;
+import ca.jrvs.apps.trading.model.domain.SecurityRow;
 import ca.jrvs.apps.trading.model.domain.Trader;
 import ca.jrvs.apps.trading.model.domain.TraderAccountView;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -44,8 +49,7 @@ public class DashboardService {
     if (traderId == null) {
       throw new IllegalArgumentException("traderId cannot be null");
     }
-    Trader trader = traderDao.findById(traderId)
-        .orElseThrow(() -> new IllegalArgumentException("traderId not found"));
+    Trader trader = findTraderByTraderId(traderId);
     Account account = findAccountByTraderId(traderId);
     TraderAccountView traderAccountView = new TraderAccountView();
     traderAccountView.setTrader(trader);
@@ -64,12 +68,27 @@ public class DashboardService {
    * @throws IllegalArgumentException if traderId is null or not found
    */
   public PortfolioView getProfileViewByTraderId(Integer traderId) {
-    // TODO
-    return null;
+    if (traderId == null) {
+      throw new IllegalArgumentException("traderId cannot be null");
+    }
+    List<Position> positions = findPositionsById(traderId);
+    PortfolioView portfolioView = new PortfolioView();
+    portfolioView.setSecurityRows(buildSecurityRows(positions));
+    return portfolioView;
   }
 
   /**
-   * Helper function to find the Account of Trader with id traderId.
+   * Helper method to find Trader with id traderId.
+   *
+   * @throws IllegalArgumentException if traderId is null or not found
+   */
+  private Trader findTraderByTraderId(Integer traderId) {
+    return traderDao.findById(traderId)
+        .orElseThrow(() -> new IllegalArgumentException("Invalid traderId"));
+  }
+
+  /**
+   * Helper method to find the Account of Trader with id traderId.
    *
    * @throws IllegalArgumentException if traderId is null or not found
    */
@@ -78,4 +97,35 @@ public class DashboardService {
         .orElseThrow(() -> new IllegalArgumentException("Invalid traderId"));
   }
 
+  /**
+   * Helper method to find List of Positions of a Trader with id traderId.
+   */
+  private List<Position> findPositionsById(Integer traderId) {
+    List<Position> allPositions = positionDao.findAll();
+    return allPositions.stream()
+        .filter(position -> position.getAccountId().equals(traderId))
+        .collect(Collectors.toList());
+  }
+
+  /**
+   * Helper method to build a List of SecurityRows from a list of Positions.
+   */
+  private List<SecurityRow> buildSecurityRows(List<Position> positions) {
+    List<SecurityRow> securityRows = new ArrayList<>();
+    positions.forEach(position -> securityRows.add(buildSecurityRow(position)));
+    return securityRows;
+  }
+
+  /**
+   * Helper method to build a SecurityRow from a Position.
+   */
+  private SecurityRow buildSecurityRow(Position position) {
+    String ticker = position.getTicker();
+    SecurityRow securityRow = new SecurityRow();
+    securityRow.setPosition(position);
+    securityRow.setQuote(quoteDao.findById(ticker)
+        .orElseThrow(() -> new IllegalArgumentException("Ticker: " + ticker + " cannot be found")));
+    securityRow.setTicker(ticker);
+    return securityRow;
+  }
 }
